29a7e34cf0cb9a0379c077cccac95842bb73a41f
JForea <ngrigorenko655@gmail.com>
2026-08-12 14:05:28 +0300

    feat: add replace function for do_sys_openat2

--- driver/src/kprobe_helpers.c
+++ driver/src/kprobe_helpers.c
@@ -1,14 +1,53 @@
 #include "kprobe_helpers.h"
+#include "rule_list.h"
 
 #include <linux/kprobes.h>
+
+#define MAX_PATH_SIZE 256
 
 static struct kprobe kp = {
     .symbol_name = "do_sys_openat2",
 };
 
+static noinline long do_sys_openat2_replacement(
+    int dfd,
+    const char __user *filename,
+    struct open_how *how
+) {
+    return -EACCES;
+}
+
 static int pre_handler(struct kprobe* p, struct pt_regs *regs) {
+    const char __user *u_filename;
+    char k_filename[MAX_PATH_SIZE];
+    long n;
+
+    u_filename = regs->si;
+
+    n = strncpy_from_user(k_filename, u_filename, MAX_PATH_SIZE);
+    if (n < 0) {
+        pr_info("failed to read filename: %ld\n", n);
+        return 0;
+    }
+
+    if (n >= sizeof(k_filename)) {
+        k_filename[sizeof(k_filename) - 1] = '\0';
+    }
+
+    if (does_match_any_rule(k_filename)) {
+        instruction_pointer_set(
+            regs,
+            (unsigned long)do_sys_openat2_replacement
+        );
+        
+        return 1;
+    }
+
     return 0;
 }
+
+NOKPROBE_SYMBOL(pre_handler);
+NOKPROBE_SYMBOL(do_sys_openat2_replacement);
 
 int kprobe_init(void) {
     int ret;
